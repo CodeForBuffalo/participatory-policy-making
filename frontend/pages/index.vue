ce0745a1c3f22d106770fbc889b5654505895427
IHIutch <jbhutch01@gmail.com>
2020-11-01 21:54:41 -0500

    Configure annotator to handle click events

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -10,7 +10,7 @@
           </ul>
         </div>
         <!-- eslint-disable-next-line -->
-        <div v-html="$md.render(srcContent.body)"></div>
+        <div id="content" v-html="$md.render(srcContent.body)"></div>
       </b-col>
     </b-row>
   </b-container>
@@ -35,23 +35,64 @@ export default {
   data() {
     return {
       annotator: null,
+      active: {},
     }
   },
   mounted() {
+    const self = this
     this.initAnnotator()
+    // document.querySelector('.annotator-hl').click((e) => {
+    //   this.annotationSelected(e)
+    // })
+    const els = document.getElementsByClassName('annotator-hl')
+    Array.from(els).forEach((el) => {
+      el.addEventListener('click', (e) => {
+        self.annotationSelected(e)
+      })
+    })
   },
   methods: {
+    onSelected(annotationId) {
+      // Return false if the id is undefined
+      if (!annotationId) {
+        return false
+      }
+      const annotation = document.querySelector(
+        `[data-annotation-id="${annotationId}"]`
+      )
+      console.log(annotation)
+    },
+    annotationSelected(e) {
+      e.stopPropagation()
+
+      // const annotationId = e.target.getAttribute('data-annotation-id')
+      const target = e.target
+      this.active.id = target.getAttribute('data-annotation-id')
+      this.active.text = target.innerHTML
+      // this.onSelected(annotationId)
+    },
     initAnnotator() {
+      const self = this
       const log = () => {
         return {
           annotationCreated(annotation) {
             console.log(annotation)
+            // const el = document.querySelector('.annotator-hl')
+            // console.log(annotation._local.highlights[0])
+            const el = annotation._local.highlights[0]
+            el.mouseover = null
+            el.addEventListener('click', (e) => {
+              self.annotationSelected(e)
+            })
           },
         }
       }
       /* eslint-disable no-undef */
       this.annotator = new annotator.App()
-      this.annotator.include(annotator.ui.main)
+      console.log(annotator.ui.tags.viewerExtension)
+      this.annotator.include(annotator.ui.main, {
+        element: document.querySelector('#content'),
+      })
       this.annotator.include(log)
       this.annotator.start()
       /* eslint-enable no-undef */
@@ -62,7 +103,21 @@ export default {
       {
         src: '/js/annotator.min.js',
       },
+      // {
+      //   src: 'https://hypothes.is/embed.js',
+      //   async: true,
+      // },
     ],
   },
 }
 </script>
+
+<style>
+.annotator-viewer {
+  display: none !important;
+}
+
+.annotator-hl {
+  cursor: pointer;
+}
+</style>
