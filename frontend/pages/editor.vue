121e4a57a5281cdae83f6bc5f6a16c13f4308cdf
IHIutch <jbhutch01@gmail.com>
2020-11-06 09:48:33 -0500

    Make buttons nicer

--- frontend/pages/editor.vue
+++ frontend/pages/editor.vue
@@ -4,127 +4,118 @@
       <b-row>
         <b-col>
           <client-only>
-            <editor-menu-bar v-slot="{ commands, isActive }" :editor="editor">
-              <div class="menubar">
-                <button
-                  class="menubar__button"
-                  :class="{ 'is-active': isActive.bold() }"
-                  @click="commands.bold"
-                >
-                  <b-icon icon="type-bold" />
-                </button>
+            <editor-menu-bar
+              v-slot="{ commands, isActive }"
+              :editor="editor"
+              class="mb-1"
+            >
+              <b-button-toolbar>
+                <b-button-group class="mr-1">
+                  <b-button :pressed="isActive.bold()" @click="commands.bold">
+                    <b-icon icon="type-bold" aria-hidden="true" />
+                  </b-button>
 
-                <button
-                  class="menubar__button"
-                  :class="{ 'is-active': isActive.italic() }"
-                  @click="commands.italic"
-                >
-                  <b-icon icon="type-italic" />
-                </button>
+                  <b-button
+                    :pressed="isActive.italic()"
+                    @click="commands.italic"
+                  >
+                    <b-icon icon="type-italic" aria-hidden="true" />
+                  </b-button>
 
-                <button
-                  class="menubar__button"
-                  :class="{ 'is-active': isActive.strike() }"
-                  @click="commands.strike"
-                >
-                  <b-icon icon="type-strikethrough" />
-                </button>
+                  <b-button
+                    :pressed="isActive.strike()"
+                    @click="commands.strike"
+                  >
+                    <b-icon icon="type-strikethrough" aria-hidden="true" />
+                  </b-button>
+                  <b-button
+                    :pressed="isActive.underline()"
+                    @click="commands.underline"
+                  >
+                    <b-icon icon="type-underline" aria-hidden="true" />
+                  </b-button>
+                </b-button-group>
 
-                <button
-                  class="menubar__button"
-                  :class="{ 'is-active': isActive.underline() }"
-                  @click="commands.underline"
-                >
-                  <b-icon icon="type-underline" />
-                </button>
+                <b-button-group class="mr-1">
+                  <b-button :pressed="isActive.code()" @click="commands.code">
+                    <b-icon icon="code" aria-hidden="true" />
+                  </b-button>
 
-                <button
-                  class="menubar__button"
-                  :class="{ 'is-active': isActive.code() }"
-                  @click="commands.code"
-                >
-                  <b-icon icon="code" />
-                </button>
+                  <b-button
+                    :pressed="isActive.paragraph()"
+                    @click="commands.paragraph"
+                  >
+                    <b-icon icon="paragraph" aria-hidden="true" />
+                  </b-button>
+                </b-button-group>
 
-                <button
-                  class="menubar__button"
-                  :class="{ 'is-active': isActive.paragraph() }"
-                  @click="commands.paragraph"
-                >
-                  <b-icon icon="paragraph" />
-                </button>
+                <b-button-group class="mr-1">
+                  <b-button
+                    :pressed="isActive.heading({ level: 1 })"
+                    @click="commands.heading({ level: 1 })"
+                  >
+                    H1
+                  </b-button>
 
-                <button
-                  class="menubar__button"
-                  :class="{ 'is-active': isActive.heading({ level: 1 }) }"
-                  @click="commands.heading({ level: 1 })"
-                >
-                  H1
-                </button>
+                  <b-button
+                    :pressed="isActive.heading({ level: 2 })"
+                    @click="commands.heading({ level: 2 })"
+                  >
+                    H2
+                  </b-button>
 
-                <button
-                  class="menubar__button"
-                  :class="{ 'is-active': isActive.heading({ level: 2 }) }"
-                  @click="commands.heading({ level: 2 })"
-                >
-                  H2
-                </button>
+                  <b-button
+                    :pressed="isActive.heading({ level: 3 })"
+                    @click="commands.heading({ level: 3 })"
+                  >
+                    H3
+                  </b-button>
+                </b-button-group>
 
-                <button
-                  class="menubar__button"
-                  :class="{ 'is-active': isActive.heading({ level: 3 }) }"
-                  @click="commands.heading({ level: 3 })"
-                >
-                  H3
-                </button>
+                <b-button-group class="mr-1">
+                  <b-button
+                    :pressed="isActive.bullet_list()"
+                    @click="commands.bullet_list"
+                  >
+                    <b-icon icon="list-ul" aria-hidden="true" />
+                  </b-button>
 
-                <button
-                  class="menubar__button"
-                  :class="{ 'is-active': isActive.bullet_list() }"
-                  @click="commands.bullet_list"
-                >
-                  <b-icon icon="list-ul" />
-                </button>
+                  <b-button
+                    :pressed="isActive.ordered_list()"
+                    @click="commands.ordered_list"
+                  >
+                    <b-icon icon="list-ol" aria-hidden="true" />
+                  </b-button>
 
-                <button
-                  class="menubar__button"
-                  :class="{ 'is-active': isActive.ordered_list() }"
-                  @click="commands.ordered_list"
-                >
-                  <b-icon icon="list-ol" />
-                </button>
+                  <b-button
+                    :pressed="isActive.blockquote()"
+                    @click="commands.blockquote"
+                  >
+                    <b-icon icon="blockquote-left" aria-hidden="true" />
+                  </b-button>
 
-                <button
-                  class="menubar__button"
-                  :class="{ 'is-active': isActive.blockquote() }"
-                  @click="commands.blockquote"
-                >
-                  <b-icon icon="blockquote-left" />
-                </button>
+                  <b-button
+                    :pressed="isActive.code_block()"
+                    @click="commands.code_block"
+                  >
+                    <b-icon icon="code" aria-hidden="true" />
+                  </b-button>
 
-                <button
-                  class="menubar__button"
-                  :class="{ 'is-active': isActive.code_block() }"
-                  @click="commands.code_block"
-                >
-                  <b-icon icon="code" />
-                </button>
+                  <b-button @click="commands.horizontal_rule">
+                    <b-icon icon="dash" aria-hidden="true" />
+                  </b-button>
+                </b-button-group>
 
-                <button
-                  class="menubar__button"
-                  @click="commands.horizontal_rule"
-                >
-                  <b-icon icon="dash" />
-                </button>
+                <b-button-group>
+                  <b-button @click="commands.undo">
+                    <b-icon icon="arrow-counterclockwise" aria-hidden="true" />
+                  </b-button>
 
-                <button class="menubar__button" @click="commands.undo">
-                  <b-icon icon="arrow-counterclockwise" />
-                </button>
-
-                <button class="menubar__button" @click="commands.redo">
-                  <b-icon icon="arrow-clockwise" />
-                </button>
-              </div>
+                  <b-button @click="commands.redo">
+                    <b-icon icon="arrow-clockwise" aria-hidden="true" />
+                  </b-button>
+                </b-button-group>
+              </b-button-toolbar>
             </editor-menu-bar>
             <editor-content :editor="editor" />
           </client-only>
